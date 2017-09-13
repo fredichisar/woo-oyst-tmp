38055bfbd4e2cad3d886d0969c24d0f5e39d8e5a
fredichisar <frederic@ichisar.com>
2017-09-13 17:48:22 +0200

    add

--- scripts/before_deploy.sh
+++ scripts/before_deploy.sh
@@ -2,16 +2,7 @@
 
 #deprecated Version
 
-mkdir travis_release
-
-cp -r * travis_release
-
-rm -f   travis_release/.travis.yml
-rm -rf  travis_release/bin
-rm -rf  travis_release/travis_release
-rm -f  travis_release/composer.*
-
-cd travis_release
+cd src
 
 zip -r woo-oyst.zip *
 tar --exclude='woo-oyst.zip' -cvzf woo-oyst.tar.gz *
