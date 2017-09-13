b8daef3dd11ca1b5023b0d76fba464fab641323d
fredichisar <frederic@ichisar.com>
2017-09-13 16:34:05 +0200

    add

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,7 +17,8 @@ PLUGIN="woo-oyst"
 PROJECT_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )/.." && pwd )"
 PLUGIN_BUILDS_PATH="$PROJECT_ROOT/builds"
 PLUGIN_BUILD_CONFIG_PATH="$PROJECT_ROOT/build-cfg"
-VERSION=$(/usr/bin/php -f "$PLUGIN_BUILD_CONFIG_PATH/utils/get_plugin_version.php" "$PROJECT_ROOT" "$PLUGIN")
+#VERSION=$(/usr/bin/php -f "$PLUGIN_BUILD_CONFIG_PATH/utils/get_plugin_version.php" "$PROJECT_ROOT" "$PLUGIN")
+VERSION="v1.0.2"
 ZIP_FILE="$PLUGIN_BUILDS_PATH/$PLUGIN-$VERSION.zip"
 
 # Ensure the zip file for the current version has been built
