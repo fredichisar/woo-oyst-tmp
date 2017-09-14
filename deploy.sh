acce25d200ac04ed4ea2eb63931bfbeb9349cf2d
fredichisar <frederic@ichisar.com>
2017-09-14 14:28:45 +0200

    simple test

--- deploy.sh
+++ deploy.sh
@@ -104,3 +104,55 @@ echo "commit to svn"
 # Remove SVN temp dir
 rm -fR svn
 echo "remove tmp dir"
+
+/////
+
+
+# Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
+PLUGINVERSION=`grep "Version:" $PLUGINDIR/$MAINFILE | awk -F' ' '{print $NF}' | tr -d '\r'`
+echo "$MAINFILE version: $PLUGINVERSION"
+READMEVERSION=`grep "^Stable tag:" $PLUGINDIR/readme.txt | awk -F' ' '{print $NF}' | tr -d '\r'`
+echo "readme.txt version: $READMEVERSION"
+
+if [ "$READMEVERSION" = "trunk" ]; then
+	echo "Version in readme.txt & $MAINFILE don't match, but Stable tag is trunk. Let's proceed..."
+elif [ "$PLUGINVERSION" != "$READMEVERSION" ]; then
+	echo "Version in readme.txt & $MAINFILE don't match. Exiting...."
+	exit 1;
+elif [ "$PLUGINVERSION" = "$READMEVERSION" ]; then
+	echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
+fi
+
+
+echo "Creating local copy of SVN repo trunk ..."
+svn checkout $SVNURL $SVNPATH --depth immediates
+svn update --quiet $SVNPATH/trunk --set-depth infinity
+
+
+
+echo "Changing directory to SVN and committing to trunk"
+cd $SVNPATH/trunk/
+# Delete all files that should not now be added.
+svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2}' | xargs svn del
+# Add all new files that are not set to be ignored
+svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
+svn commit --username=$SVNUSER --password=$SVNPWD -m "Preparing for $PLUGINVERSION release"
+
+
+
+echo "Creating new SVN tag and committing it"
+cd $SVNPATH
+svn update --quiet $SVNPATH/tags/$PLUGINVERSION
+svn copy --quiet trunk/ tags/$PLUGINVERSION/
+# Remove assets and trunk directories from tag directory
+svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/assets
+svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/trunk
+cd $SVNPATH/tags/$PLUGINVERSION
+svn commit --username=$SVNUSER -m "Tagging version $PLUGINVERSION"
+
+echo "Removing temporary directory $SVNPATH"
+cd $SVNPATH
+cd ..
+rm -fr $SVNPATH/
+
+echo "*** FIN ***"
--- to_svn.sh
+++ to_svn.sh
@@ -1,7 +1,5 @@
 #! /bin/bash
 
-
-# Set up some default values. Feel free to change these in your own script
 CURRENTDIR=`pwd`
 PLUGINSLUG="woo-oyst"
 SVNPATH="/tmp/$PLUGINSLUG"
@@ -11,60 +9,11 @@ SVNPWD=WP_ORG_PASSWORD
 PLUGINDIR=CURRENTDIR
 MAINFILE="$PLUGINSLUG.php"
 
-
-# Let's begin...
-echo ".........................................."
-echo
-echo "Preparing to deploy WordPress plugin"
-echo
-echo ".........................................."
-echo
-
-# Check version in readme.txt is the same as plugin file after translating both to unix line breaks to work around grep's failure to identify mac line breaks
-PLUGINVERSION=`grep "Version:" $PLUGINDIR/$MAINFILE | awk -F' ' '{print $NF}' | tr -d '\r'`
-echo "$MAINFILE version: $PLUGINVERSION"
-READMEVERSION=`grep "^Stable tag:" $PLUGINDIR/readme.txt | awk -F' ' '{print $NF}' | tr -d '\r'`
-echo "readme.txt version: $READMEVERSION"
-
-if [ "$READMEVERSION" = "trunk" ]; then
-	echo "Version in readme.txt & $MAINFILE don't match, but Stable tag is trunk. Let's proceed..."
-elif [ "$PLUGINVERSION" != "$READMEVERSION" ]; then
-	echo "Version in readme.txt & $MAINFILE don't match. Exiting...."
-	exit 1;
-elif [ "$PLUGINVERSION" = "$READMEVERSION" ]; then
-	echo "Versions match in readme.txt and $MAINFILE. Let's proceed..."
-fi
-
-
-echo "Creating local copy of SVN repo trunk ..."
-svn checkout $SVNURL $SVNPATH --depth immediates
-svn update --quiet $SVNPATH/trunk --set-depth infinity
-
-
-
-echo "Changing directory to SVN and committing to trunk"
-cd $SVNPATH/trunk/
-# Delete all files that should not now be added.
-svn status | grep -v "^.[ \t]*\..*" | grep "^\!" | awk '{print $2}' | xargs svn del
-# Add all new files that are not set to be ignored
-svn status | grep -v "^.[ \t]*\..*" | grep "^?" | awk '{print $2}' | xargs svn add
-svn commit --username=$SVNUSER --password=$SVNPWD -m "Preparing for $PLUGINVERSION release"
-
-
-
-echo "Creating new SVN tag and committing it"
-cd $SVNPATH
-svn update --quiet $SVNPATH/tags/$PLUGINVERSION
-svn copy --quiet trunk/ tags/$PLUGINVERSION/
-# Remove assets and trunk directories from tag directory
-svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/assets
-svn delete --force --quiet $SVNPATH/tags/$PLUGINVERSION/trunk
-cd $SVNPATH/tags/$PLUGINVERSION
-svn commit --username=$SVNUSER -m "Tagging version $PLUGINVERSION"
-
-echo "Removing temporary directory $SVNPATH"
-cd $SVNPATH
-cd ..
-rm -fr $SVNPATH/
-
-echo "*** FIN ***"
+echo CURRENTDIR
+echo PLUGINSLUG
+echo SVNPATH
+echo SVNURL
+echo SVNUSER
+echo SVNPWD
+echo PLUGINDIR
+echo MAINFILE
